27a4f950a43042dae4214ed9e976ebbe59e92a08
Sam Given <sgiven96@gmail.com>
2017-10-04 01:26:10 -0400

    adding indexing scripts for both of the indices of the student_teacher dataset

---
+++ data-sets/scripts/json_index.py
@@ -0,0 +1,30 @@
+import json
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+es = Elasticsearch()
+
+path = '../student_teacher/MOCK_DATA.json'
+
+f=open(path, 'r')
+content = f.readlines()
+data = ''.join(content)
+data = json.loads(data)
+f.close()
+
+# i=0
+# for item in data:
+# 	doc = item
+# 	res = es.index(index='students', doc_type='student', id=doc['id'], body=doc)
+# 	i+=1
+
+actions = [
+	{
+		'_index': 'students',
+		'_type': 'student',
+		'_id': doc['id'],
+		'_source': doc
+	}
+	for doc in data
+]
+
+helpers.bulk(es, actions)
