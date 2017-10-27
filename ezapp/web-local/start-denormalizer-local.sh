3fec0089c24d53568f66e4ff1e457cb95aede787
bdevore17 <bdevore17@gmail.com>
2017-10-27 17:55:34 -0400

    first basic setup for samza in ezapp

---
+++ ezapp/web-local/start-denormalizer-local.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BASE_DIR=$(dirname $DIR)
+CLEAN=
+VERSION="0.1"
+DENORMALIZER_DIR=${BASE_DIR}/denormalizer
+
+java -Dlog4j.configuration=file:${DIR}/log4j-console.xml -Dsamza.log.dir=${DIR} -jar ${DENORMALIZER_DIR}/target/ezapp-denormalizer-${VERSION}-SNAPSHOT.jar -config-path ${DENORMALIZER_DIR}/src/main/config/fanout-app-local-runner.properties --config-factory org.apache.samza.config.factories.PropertiesConfigFactory
