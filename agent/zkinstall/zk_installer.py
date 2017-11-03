97a1864762e6fed16967a27c19762578a91f05de
Beshoi G <bgenidy@gwmail.gwu.edu>
2017-11-02 17:23:56 -0700

    init zookeeper install

---
+++ agent/zkinstall/zk_installer.py
@@ -0,0 +1,16 @@
+"""
+Installs Zookeeper on Ubuntu 16.04
+"""
+
+import os
+import wget
+
+zk_file = 'zookeeper-3.3.6.tar.gz'
+ezstack_dir = '/opt/ezstack'
+
+cmd = os.system
+cmd('mkdir -p {}'.format(ezstack_dir))
+wget.download('http://mirror.metrocast.net/apache/zookeeper/zookeeper-3.3.6/{}'.format(zk_file),
+              out='{}/{}'.format(zk_file))
+cmd('tar xvzf {}/{}'.format(ezstack_dir, zk_file))
+
