1bba81a57a74b8fd8d42f266167dd03296056b61
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2017-11-06 21:57:10 -0800

    started mesos agent integration

--- agent/ubuntu_app_installer.sh
+++ agent/ubuntu_app_installer.sh
@@ -3,3 +3,6 @@ sudo apt-get update
 sudo apt-get install -y default-jdk
 sudo apt-get install -y python
 sudo apt-get install -y python-pip
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get -y install build-essential python-dev libcurl4-nss-dev libsasl2-dev libsasl2-modules maven libapr1-dev libsvn-dev
+sudo apt-get install -y unzip
