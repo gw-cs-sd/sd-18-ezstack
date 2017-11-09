091731101c0a89431a45723a5c68bdd350d47b9a
Sam Given <sgiven96@gmail.com>
2017-11-09 00:44:57 -0500

    adding requirements file

--- data-sets/scripts/json_index.py
+++ data-sets/scripts/json_index.py
@@ -1,20 +1,26 @@
-import json
+import json, requests
 from elasticsearch import Elasticsearch
 from elasticsearch import helpers
 es = Elasticsearch()
 
 path = '../student_teacher/MOCK_DATA.json'
 
+headers = {
+    'Content-Type': 'application/json',
+}
+
 f=open(path, 'r')
 content = f.readlines()
 data = ''.join(content)
 data = json.loads(data)
 f.close()
+table = ''
 
 i=0
-for item in data:
- 	doc = item
- 	res = es.index(index='students', doc_type='student', id=doc['id'], body=doc)
+for doc in data:
+ 	info = '{"author":"Ben and Sam","title":"Chris is awesome","rating":290}'
+ 	#res = es.index(index='students', doc_type='student', id=doc['id'], body=doc)
+ 	requests.post('http://localhost:8080/sor/1/{}/{}'.format(table, doc['id']), headers=headers, data=info)
  	i+=1
 
 #actions = [
