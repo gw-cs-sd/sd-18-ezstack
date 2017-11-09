540253c28e87f98c54d2e5f5a9ee5ec447a78c2f
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2017-11-08 21:19:00 -0800

    samza executor

--- samza-mesos/src/main/java/org/ezstack/samza/SamzaExecutor.java
+++ samza-mesos/src/main/java/org/ezstack/samza/SamzaExecutor.java
@@ -3,18 +3,23 @@ package org.ezstack.samza;
 import org.apache.mesos.Executor;
 import org.apache.mesos.ExecutorDriver;
 import org.apache.mesos.Protos;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SamzaExecutor implements Executor {
-    public void registered(ExecutorDriver executorDriver, Protos.ExecutorInfo executorInfo, Protos.FrameworkInfo frameworkInfo, Protos.SlaveInfo slaveInfo) {
+    private static final Logger LOG = LoggerFactory.getLogger(SamzaExecutor.class);
 
+    public void registered(ExecutorDriver executorDriver, Protos.ExecutorInfo executorInfo,
+                           Protos.FrameworkInfo frameworkInfo, Protos.SlaveInfo slaveInfo) {
+        LOG.info("registered slave on " + slaveInfo.getHostname());
     }
 
     public void reregistered(ExecutorDriver executorDriver, Protos.SlaveInfo slaveInfo) {
-
+        LOG.info("reregistered slave on " + slaveInfo.getHostname());
     }
 
     public void disconnected(ExecutorDriver executorDriver) {
-
+        LOG.info("disconnected");
     }
 
     public void launchTask(ExecutorDriver executorDriver, Protos.TaskInfo taskInfo) {
@@ -22,18 +27,18 @@ public class SamzaExecutor implements Executor {
     }
 
     public void killTask(ExecutorDriver executorDriver, Protos.TaskID taskID) {
-
+        LOG.info("task " + taskID.getValue() + " killed");
     }
 
     public void frameworkMessage(ExecutorDriver executorDriver, byte[] bytes) {
-
+        LOG.info("framework message: " + new String(bytes));
     }
 
     public void shutdown(ExecutorDriver executorDriver) {
-
+        LOG.info("shutdown");
     }
 
     public void error(ExecutorDriver executorDriver, String s) {
-
+        LOG.error("error: " + s);
     }
 }
