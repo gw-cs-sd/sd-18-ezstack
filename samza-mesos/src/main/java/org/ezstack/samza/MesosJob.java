9785ac02da6ede6e0a0a96aae9c6ae72064cb364
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2017-11-08 16:28:38 -0800

    more code

--- samza-mesos/src/main/java/org/ezstack/samza/MesosJob.java
+++ samza-mesos/src/main/java/org/ezstack/samza/MesosJob.java
@@ -1,24 +1,35 @@
 package org.ezstack.samza;
 
+import org.apache.mesos.MesosSchedulerDriver;
+import org.apache.mesos.Protos;
 import org.apache.samza.config.Config;
 import org.apache.samza.job.ApplicationStatus;
 import org.apache.samza.job.StreamJob;
 
 public class MesosJob implements StreamJob {
+    private ApplicationStatus applicationStatus;
     private MesosConfig mesosConfig;
+    private MesosSchedulerDriver mesosSchedulerDriver;
     private SamzaScheduler samzaScheduler;
     private SamzaExecutor samzaExecutor;
 
     public MesosJob(Config config) {
         mesosConfig = new MesosConfig(config);
+        samzaScheduler = new SamzaScheduler(); // Constructor will change
+        mesosSchedulerDriver = new MesosSchedulerDriver(samzaScheduler,
+                getFrameWorkInfo(), mesosConfig.getMasterConnect());
+        applicationStatus = ApplicationStatus.New;
     }
 
     public StreamJob submit() {
-        return null;
+        // TODO
+        applicationStatus = ApplicationStatus.Running;
+        return this;
     }
 
     public StreamJob kill() {
-        return null;
+        // TODO
+        return this;
     }
 
     public ApplicationStatus waitForFinish(long l) {
@@ -30,6 +41,10 @@ public class MesosJob implements StreamJob {
     }
 
     public ApplicationStatus getStatus() {
+        return applicationStatus;
+    }
+
+    private static Protos.FrameworkInfo getFrameWorkInfo() {
         return null;
     }
 }
