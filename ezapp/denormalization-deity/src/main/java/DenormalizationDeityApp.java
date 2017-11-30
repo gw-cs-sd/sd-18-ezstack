85e4a97073cd5d56b1d73740b05f4217ea8244e3
Sam Given <sgiven96@gmail.com>
2017-11-29 21:09:43 -0500

    changes

--- ezapp/denormalization-deity/src/main/java/DenormalizationDeityApp.java
+++ ezapp/denormalization-deity/src/main/java/DenormalizationDeityApp.java
@@ -10,17 +10,24 @@ import java.util.Map;
 public class DenormalizationDeityApp implements StreamApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DenormalizationDeityApp.class);
+    private long numQueries = 0;
+    private long totalResponseTime = 0;
+    private double average = 0;
 
     @Override
     public void init(StreamGraph streamGraph, Config config) {
-        MessageStream<Map<String, Object>> queries = streamGraph.<String, Map<String, Object>, Map<String, Object>>getInputStream("queries", (key, msg) -> msg);
+        MessageStream<Map<String, Object>> queryStream = streamGraph.<String, Map<String, Object>, Map<String, Object>>getInputStream("queries", (key, msg) -> msg);
 
-        queries.map(this::printMessage);
+        queryStream.map(this::processQuery);
     }
 
-    private Map<String, Object> printMessage(Map<String, Object> msg) {
-        log.info(msg.toString());
-        return msg;
+    private Map<String, Object> processQuery(Map<String, Object> query) {
+        log.info(query.toString());
+        numQueries++;
+        totalResponseTime += new Long(((Map) query.get("_data")).get("responseTime").toString()).longValue();
+        average = totalResponseTime / numQueries;
+        log.info("Average:" + average);
+        return query;
     }
 
 }
