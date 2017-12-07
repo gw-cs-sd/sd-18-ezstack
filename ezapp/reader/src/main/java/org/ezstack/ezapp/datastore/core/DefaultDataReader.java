9f5457b04e63eb49cdb4f574db6f1c0b9e32afd4
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2017-12-06 20:33:06 -0800

    _dataReader -> _dataReaderDAO

--- ezapp/reader/src/main/java/org/ezstack/ezapp/datastore/core/DefaultDataReader.java
+++ ezapp/reader/src/main/java/org/ezstack/ezapp/datastore/core/DefaultDataReader.java
@@ -10,22 +10,22 @@ import java.util.List;
 import java.util.Map;
 
 public class DefaultDataReader implements DataReader {
-    private final ElasticSearchDataReaderDAO _dataReader;
+    private final ElasticSearchDataReaderDAO _dataReaderDAO;
 
     @Inject
     public DefaultDataReader(ElasticSearchDataReaderDAO dataReader) {
         Preconditions.checkNotNull(dataReader);
 
-        _dataReader = dataReader;
+        _dataReaderDAO = dataReader;
     }
 
     @Override
     public Map<String, Object> getDocument(String database, String table, String id) {
-        return _dataReader.getDocument(database, table, id);
+        return _dataReaderDAO.getDocument(database, table, id);
     }
 
     @Override
     public List<Map<String, Object>> getDocuments(Query query) {
-        return _dataReader.getDocuments(query);
+        return _dataReaderDAO.getDocuments(query);
     }
 }
