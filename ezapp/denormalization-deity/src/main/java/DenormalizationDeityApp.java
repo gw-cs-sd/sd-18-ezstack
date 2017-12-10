804858cff19bbaa151b00530b39b81f7734346aa
Sam Given <sgiven96@gmail.com>
2017-12-10 18:55:54 -0500

    Implemented metics using Dropwizard metrics
    Using drop wizard metrics, we can maintain percentile based information
    without storing values, and it will dynamically weight the values
    towards the most recent query response times.

--- ezapp/denormalization-deity/src/main/java/DenormalizationDeityApp.java
+++ ezapp/denormalization-deity/src/main/java/DenormalizationDeityApp.java
@@ -1,32 +1,43 @@
+import com.codahale.metrics.Histogram;
+import com.codahale.metrics.MetricRegistry;
 import org.apache.samza.application.StreamApplication;
 import org.apache.samza.config.Config;
 import org.apache.samza.operators.MessageStream;
 import org.apache.samza.operators.StreamGraph;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.coursera.metrics.datadog.DatadogReporter;
+import org.coursera.metrics.datadog.DatadogReporter.Expansion;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.coursera.metrics.datadog.transport.HttpTransport;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class DenormalizationDeityApp implements StreamApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DenormalizationDeityApp.class);
-    private long numQueries = 0;
-    private long totalResponseTime = 0;
-    private double average = 0;
+    private final MetricRegistry metrics = new MetricRegistry();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void init(StreamGraph streamGraph, Config config) {
-        MessageStream<Map<String, Object>> queryStream = streamGraph.<String, Map<String, Object>, Map<String, Object>>getInputStream("queries", (key, msg) -> msg);
-
+        MessageStream<Query> queryStream = streamGraph.<String, Map<String, Object>, Query>getInputStream("queries", this::convertToQuery);
         queryStream.map(this::processQuery);
+
+        HttpTransport transport = new HttpTransport.Builder().withApiKey("c8c1d236dc42e0acfdec46ed8614d20e").build();
+        DatadogReporter reporter = DatadogReporter.forRegistry(metrics).withTransport(transport).withExpansions(Expansion.ALL).build();
+
+        reporter.start(10, TimeUnit.SECONDS);
     }
 
-    private Map<String, Object> processQuery(Map<String, Object> query) {
-        log.info(query.toString());
-        numQueries++;
-        totalResponseTime += new Long(((Map) query.get("_data")).get("responseTime").toString()).longValue();
-        average = totalResponseTime / numQueries;
-        log.info("Average:" + average);
+    private Query convertToQuery(String key, Map<String, Object> msg) {
+        return objectMapper.convertValue(msg, Query.class);
+    }
+
+    private Query processQuery(Query query) {
+        Histogram histogram = metrics.histogram(query.getStrippedQuery());
+        histogram.update(query.getResponseTime());
         return query;
     }
 
