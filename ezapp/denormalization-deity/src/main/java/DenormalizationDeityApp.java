746017431d1124675af0ab61a1e19e998a2002e1
Sam Given <sgiven96@gmail.com>
2018-01-28 23:05:07 -0500

    Now has correct efficiency algorithm
    Not fully functional yet, still needs to handle rule allocation

--- ezapp/denormalization-deity/src/main/java/DenormalizationDeityApp.java
+++ ezapp/denormalization-deity/src/main/java/DenormalizationDeityApp.java
@@ -1,5 +1,7 @@
 import com.codahale.metrics.Histogram;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Snapshot;
+import com.codahale.metrics.UniformReservoir;
 import org.apache.samza.application.StreamApplication;
 import org.apache.samza.config.Config;
 import org.apache.samza.operators.MessageStream;
@@ -11,14 +13,26 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.coursera.metrics.datadog.transport.HttpTransport;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class DenormalizationDeityApp implements StreamApplication {
 
     private static final Logger log = LoggerFactory.getLogger(DenormalizationDeityApp.class);
-    private final MetricRegistry metrics = new MetricRegistry();
-    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final MetricRegistry metrics;
+    private final ObjectMapper objectMapper;
+    private final MetricRegistry.MetricSupplier<Histogram> histogramSupplier;
+    private Map<String, QueryObject> priorityObjects;
+
+    public DenormalizationDeityApp() {
+        metrics = new MetricRegistry();
+        objectMapper = new ObjectMapper();
+        histogramSupplier = () -> new Histogram(new UniformReservoir());
+        priorityObjects = new HashMap<>();
+    }
 
     @Override
     public void init(StreamGraph streamGraph, Config config) {
@@ -37,9 +51,54 @@ public class DenormalizationDeityApp implements StreamApplication {
     }
 
     private Query processQuery(Query query) {
-        Histogram histogram = metrics.histogram(query.getStrippedQuery());
+        String strippedQuery = query.getStrippedQuery();
+
+        Histogram histogram = metrics.histogram(strippedQuery, histogramSupplier);
         histogram.update(query.getResponseTime());
+
+        updateQueryObject(strippedQuery);
+
         return query;
     }
 
+    private void updateQueryObject(String strippedQuery) { //Priority is represented as the Median, skewed by the mean absolute deviation from the median
+        Histogram histogram = metrics.histogram(strippedQuery, histogramSupplier);
+        Snapshot snap = histogram.getSnapshot();
+
+        long mean = (long) snap.getMean();
+        long median = (long) snap.getMedian();
+
+        long[] values = snap.getValues();
+        long meanAbsoluteDeviation = 0;
+
+        long priority;
+
+        for (int i = 0; i < values.length; i++) {
+            meanAbsoluteDeviation += Math.abs(values[i]-median);
+        }
+        meanAbsoluteDeviation = meanAbsoluteDeviation/mean;
+
+        if(median <= mean) {
+            priority = median - meanAbsoluteDeviation;
+        }
+        else {
+            priority = median + meanAbsoluteDeviation;
+        }
+
+        Date timestamp = new Date();
+        long stamp = timestamp.getTime();
+
+        QueryObject queryObject;
+
+        if (priorityObjects.containsKey(strippedQuery)) {
+            queryObject = priorityObjects.get(strippedQuery);
+            queryObject.setRecentTimestamp(stamp);
+            queryObject.setPriority(priority);
+        }
+        else {
+            queryObject = new QueryObject(strippedQuery, priority, stamp);
+        }
+
+        priorityObjects.put(strippedQuery, queryObject);
+    }
 }
