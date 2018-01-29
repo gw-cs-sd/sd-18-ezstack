4679eb93aa891c5cfe65e0607fffb0fade34896d
Ben Devore <bdevore@gwu.edu>
2018-01-29 00:16:38 -0500

    Document Resolver Deduplication (#26)
    * new dedup document implementation
    
    * remove unused dependency
    
    * Updated unit tests for Document
    
    Found one bug inside of document, it is now fixed.
    
    * complete dedup
    
    * reformat comments

--- ezapp/datastore-api/src/main/java/org/ezstack/ezapp/datastore/api/Document.java
+++ ezapp/datastore-api/src/main/java/org/ezstack/ezapp/datastore/api/Document.java
@@ -1,10 +1,10 @@
 package org.ezstack.ezapp.datastore.api;
 
-import com.datastax.driver.core.utils.UUIDs;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.MoreObjects;
+import com.fasterxml.jackson.databind.util.ISO8601Utils;
 
+import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 
@@ -13,66 +13,104 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 public class Document {
 
+    private static final long NUM_100NS_INTERVALS_SINCE_UUID_EPOCH = 0x01b21dd213814000L;
+
     private final String _table;
     private final String _key;
-    private UUID _timestamp;
+    private final UUID _firstUpdateAt;
+    private UUID _lastUpdateAt;
     private Map<String, Object> _data;
     private int _version;
 
+    private boolean _hasMutated;
+
     @JsonCreator
-    public Document(@JsonProperty("_table") String table,
-                    @JsonProperty("_key") String key, @JsonProperty("_timestamp") UUID timestamp,
+    public Document(@JsonProperty("_table") String table, @JsonProperty("_key") String key,
+                    @JsonProperty("_firstUpdateAt") UUID firstUpdateAt, @JsonProperty("_lastUpdateAt") UUID lastUpdateAt,
                     @JsonProperty("_data") Map<String, Object> data, @JsonProperty("_version") int version) {
 
         checkNotNull(table, "table");
         checkNotNull(key, "key");
         checkNotNull(data, "data");
+        checkNotNull(firstUpdateAt, "firstUpdateAt");
+        checkNotNull(lastUpdateAt, "lastUpdateAt");
         checkArgument(Names.isLegalTableName(table), "Invalid Table Name");
         checkArgument(Names.isLegalKey("Invalid key"));
 
         _table = table;
         _key = key;
-        _timestamp = MoreObjects.firstNonNull(timestamp, UUIDs.timeBased());
+        _firstUpdateAt = firstUpdateAt;
+        _lastUpdateAt = lastUpdateAt;
         _data = data;
         _version = version;
+
+        _hasMutated = false;
     }
 
-    // This constructor should only be used if there is only one version of this document to consider
+    /** This constructor should only be used if there is only one version of this document to consider */
     public Document(Update update) {
         _table = update.getTable();
         _key = update.getKey();
-        _timestamp = update.getTimestamp();
+        _firstUpdateAt = update.getTimestamp();
+        _lastUpdateAt = update.getTimestamp();
         _data = update.getData();
         _version = 1;
+
+        _hasMutated = false;
     }
 
+    /** lastUpdateAt and version are only updated if the document data is actually modified by the update */
     public void addUpdate(Update update) {
         checkArgument(_table.equals(update.getTable()) && _key.equals(update.getKey()),
                 "Update is not to same record as existing document");
 
-        // TODO: figure out a better way to maintain timestamp consistency, until then, simple assignment
-        _timestamp = update.getTimestamp();
-
-        _version++;
-
         if (update.isUpdate()) {
-            resolveData(update.getData(), _data);
+            resolveData(update.getData(), _data, update.getTimestamp());
         } else {
             _data = update.getData();
+            _hasMutated = true;
+        }
+
+        if(_hasMutated) {
+            _version++;
+            _lastUpdateAt = update.getTimestamp();
+            _hasMutated = false;
         }
     }
 
-    private static void resolveData(Map<String, Object> updatedData, Map<String, Object> dataToUpdate) {
-        for (Map.Entry<String, Object> attribute : updatedData.entrySet()) {
+    private void resolveData(Map<String, Object> updatedData, Map<String, Object> dataToUpdate, UUID updatedDataTimestamp) {
 
-            // TODO: define a better write model to conditionally update parts of a nested document, until then merge them attribute by attribute
-            if (attribute.getValue() instanceof Map<?, ?> && dataToUpdate.get(attribute.getKey()) instanceof Map<?,?>) {
-                resolveData((Map<String, Object>) attribute.getValue(), (Map<String, Object>) dataToUpdate.get(attribute.getKey()));
+        for (Map.Entry<String, Object> attribute : updatedData.entrySet()) {
+            Object updatedVal = attribute.getValue();
+            Object oldVal = dataToUpdate.get(attribute.getKey());
+
+            if (DataType.getDataType(updatedVal) == DataType.JsonTypes.MAP && DataType.getDataType(oldVal) == DataType.JsonTypes.MAP) {
+                resolveData((Map<String, Object>) updatedVal, (Map<String, Object>) oldVal, updatedDataTimestamp);
             } else {
-                dataToUpdate.put(attribute.getKey(), attribute.getValue());
+                if (updatedVal != null ? !updatedVal.equals(oldVal) : updatedVal != oldVal) {
+                    _hasMutated = true;
+                    dataToUpdate.put(attribute.getKey(), updatedVal);
+                }
             }
         }
 
+    }
+
+
+    private static String asISOTimestamp(UUID timeUuid) {
+        return (timeUuid != null) ? ISO8601Utils.format(new Date(getTimeMillis(timeUuid)), true) : null;
+    }
+
+    private static long getTimeMillis(UUID uuid) {
+        return (uuid.timestamp() - NUM_100NS_INTERVALS_SINCE_UUID_EPOCH) / 10000;
+    }
+
+    public String getFirstUpdateAt() {
+        return asISOTimestamp(_firstUpdateAt);
+    }
+
+    public String getLastUpdateAt() {
+        return asISOTimestamp(_lastUpdateAt);
     }
 
     @JsonProperty("_table")
@@ -85,9 +123,14 @@ public class Document {
         return _key;
     }
 
-    @JsonProperty("_timestamp")
-    public UUID getTimestamp() {
-        return _timestamp;
+    @JsonProperty("_firstUpdateAt")
+    public UUID getFirstUpdateAtUUID() {
+        return _firstUpdateAt;
+    }
+
+    @JsonProperty("_lastUpdateAt")
+    public UUID getLastUpdateAtUUID() {
+        return _lastUpdateAt;
     }
 
     @JsonProperty("_data")
--- ezapp/datastore-api/src/test/java/org/ezstack/ezapp/datastore/api/DocumentTest.java
+++ ezapp/datastore-api/src/test/java/org/ezstack/ezapp/datastore/api/DocumentTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -14,7 +15,7 @@ import java.util.UUID;
 
 public class DocumentTest {
 
-    private final String jsonDoc = "{\"_table\":\"comment\",\"_key\":\"dsfaf3\",\"_timestamp\":\"90cec5d0-c8b7-11e7-8a28-ff35eed18bcb\",\"_data\":{\"author\":{\"firstName\":\"Bob\",\"lastName\":\"Johnson\"},\"title\":\"Best Ever!\",\"likes\":50},\"_version\":2}";
+    private final String jsonDoc = "{\"_table\":\"comment\",\"_key\":\"dsfaf3\",\"_firstUpdateAt\":\"90cec5d0-c8b7-11e7-8a28-ff35eed18bcb\",\"_lastUpdateAt\":\"90cec5d0-c8b7-11e7-8a28-ff35eed18bcb\",\"_data\":{\"author\":{\"firstName\":\"Bob\",\"lastName\":\"Johnson\"},\"title\":\"Best Ever!\",\"likes\":50},\"_version\":2}";
     private Document document;
     private ObjectMapper mapper;
 
@@ -29,15 +30,17 @@ public class DocumentTest {
 
         assertEquals("comment", document.getTable());
         assertEquals("dsfaf3", document.getKey());
-        assertEquals(UUID.fromString("90cec5d0-c8b7-11e7-8a28-ff35eed18bcb"), document.getTimestamp());
+        assertEquals(UUID.fromString("90cec5d0-c8b7-11e7-8a28-ff35eed18bcb"), document.getFirstUpdateAtUUID());
+        assertEquals("2017-11-13T21:13:59.213Z", document.getFirstUpdateAt());
+        assertEquals(UUID.fromString("90cec5d0-c8b7-11e7-8a28-ff35eed18bcb"), document.getLastUpdateAtUUID());
+        assertEquals("2017-11-13T21:13:59.213Z", document.getLastUpdateAt());
         assertEquals(2, document.getVersion());
-        assertEquals("{\"author\":{\"firstName\":\"Bob\",\"lastName\":\"Johnson\"},\"title\":\"Best Ever!\",\"likes\":50}",
-                mapper.writeValueAsString(document.getData()));
+        assertEquals("{\"author\":{\"firstName\":\"Bob\",\"lastName\":\"Johnson\"},\"title\":\"Best Ever!\",\"likes\":50}", mapper.writeValueAsString(document.getData()));
     }
 
     @Test
     public void testUpdateConstructor() {
-        UUID timestamp = UUIDs.timeBased();
+        UUID timestamp = UUID.fromString("1c8f95b0-0263-11e8-8f1a-0800200c9a66");
         Map<String, Object> data = new HashMap<>();
         data.put("likes", 25);
         data.put("author", "Bob");
@@ -47,14 +50,42 @@ public class DocumentTest {
 
         assertEquals("posts", doc.getTable());
         assertEquals("abc123", doc.getKey());
-        assertEquals(timestamp, doc.getTimestamp());
+        assertEquals(UUID.fromString("90cec5d0-c8b7-11e7-8a28-ff35eed18bcb"), document.getFirstUpdateAtUUID());
+        assertEquals("2017-11-13T21:13:59.213Z", document.getFirstUpdateAt());
+        assertEquals(timestamp, doc.getLastUpdateAtUUID());
+        assertEquals("2017-11-13T21:13:59.213Z", document.getLastUpdateAt());
         assertEquals(1, doc.getVersion());
         assertEquals(data, doc.getData());
     }
 
     @Test
+    public void testRedundantUpdate() {
+        UUID timestamp = UUID.fromString("1c8f95b0-0263-11e8-8f1a-0800200c9a66");
+        Map<String, Object> data = new HashMap<>();
+        Map<String, Object> nested = new HashMap<>();
+        nested.put("firstName", "Bob");
+        nested.put("lastName", "Johnson");
+        data.put("author", nested);
+        data.put("title", "Best Ever!");
+        data.put("likes", 50);
+
+        Update update = new Update("comment", "dsfaf3", timestamp, data, true);
+
+        document.addUpdate(update);
+
+        assertEquals("comment", document.getTable());
+        assertEquals("dsfaf3", document.getKey());
+        assertEquals(UUID.fromString("90cec5d0-c8b7-11e7-8a28-ff35eed18bcb"), document.getFirstUpdateAtUUID());
+        assertEquals("2017-11-13T21:13:59.213Z", document.getFirstUpdateAt());
+        assertNotEquals(timestamp, document.getLastUpdateAtUUID());
+        assertEquals("2017-11-13T21:13:59.213Z", document.getLastUpdateAt());
+        assertEquals(2, document.getVersion());
+        assertEquals(data, document.getData());
+    }
+
+    @Test
     public void testAddUpdateWithSmashUpdate() {
-        UUID timestamp = UUIDs.timeBased();
+        UUID timestamp = UUID.fromString("1c8f95b0-0263-11e8-8f1a-0800200c9a66");
         Map<String, Object> data = new HashMap<>();
         data.put("likes", 25);
         data.put("author", "Bob");
@@ -64,14 +95,17 @@ public class DocumentTest {
 
         assertEquals("comment", document.getTable());
         assertEquals("dsfaf3", document.getKey());
-        assertEquals(timestamp, document.getTimestamp());
+        assertEquals(UUID.fromString("90cec5d0-c8b7-11e7-8a28-ff35eed18bcb"), document.getFirstUpdateAtUUID());
+        assertEquals("2017-11-13T21:13:59.213Z", document.getFirstUpdateAt());
+        assertEquals(timestamp, document.getLastUpdateAtUUID());
+        assertEquals("2018-01-26T06:35:33.899Z", document.getLastUpdateAt());
         assertEquals(3, document.getVersion());
         assertEquals(data, document.getData());
     }
 
     @Test
     public void testAddUpdateWithMerge() {
-        UUID timestamp = UUIDs.timeBased();
+        UUID timestamp = UUID.fromString("1c8f95b0-0263-11e8-8f1a-0800200c9a66");;
         Map<String, Object> data = new HashMap<>();
         Map<String, Object> author = new HashMap<>();
         data.put("likes", 25);
@@ -98,7 +132,10 @@ public class DocumentTest {
 
         assertEquals("comment", document.getTable());
         assertEquals("dsfaf3", document.getKey());
-        assertEquals(timestamp, document.getTimestamp());
+        assertEquals(UUID.fromString("90cec5d0-c8b7-11e7-8a28-ff35eed18bcb"), document.getFirstUpdateAtUUID());
+        assertEquals("2017-11-13T21:13:59.213Z", document.getFirstUpdateAt());
+        assertEquals(timestamp, document.getLastUpdateAtUUID());
+        assertEquals("2018-01-26T06:35:33.899Z", document.getLastUpdateAt());
         assertEquals(3, document.getVersion());
         assertEquals(expectedDataAfterMerge, document.getData());
     }
