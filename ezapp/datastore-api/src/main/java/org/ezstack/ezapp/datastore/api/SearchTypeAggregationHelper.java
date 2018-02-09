0f9496e44b6aff8d632ec46d89b9914fa9032dec
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2018-02-08 21:30:06 -0800

    more aggregation helper stuff

--- ezapp/datastore-api/src/main/java/org/ezstack/ezapp/datastore/api/SearchTypeAggregationHelper.java
+++ ezapp/datastore-api/src/main/java/org/ezstack/ezapp/datastore/api/SearchTypeAggregationHelper.java
@@ -18,7 +18,23 @@ public class SearchTypeAggregationHelper {
     }
 
     public void computeDocument(Map<String, Object> doc) {
-        // TODO
+        switch (_searchType.getType()) {
+            case COUNT:
+                count(doc);
+                break;
+            case MAX:
+                max(doc);
+                break;
+            case MIN:
+                min(doc);
+                break;
+            case SUM:
+                sum(doc);
+                break;
+            case AVG:
+                avg(doc);
+                break;
+        }
     }
 
     public Object getResult() {
@@ -32,8 +48,50 @@ public class SearchTypeAggregationHelper {
         }
     }
 
-    private void detectAndChangeTypeValue(Object value) {
+    private void count(Map<String, Object> doc) {
         // TODO
+    }
+
+    private void max(Map<String, Object> doc) {
+        // TODO
+    }
+
+    private void min(Map<String, Object> doc) {
+        // TODO
+    }
+
+    private void sum(Map<String, Object> doc) {
+        // TODO
+    }
+
+    private void avg(Map<String, Object> doc) {
+        // TODO
+    }
+
+    /**
+     * method detects value type and sets the type appropriately.
+     *
+     * if for some reason it detects that the type has changed from integer to double then it will
+     * migrate the current results onto the double value and clear the integer value out.
+     *
+     * no such check is done from double to int because we never convert values to a lower precision.
+     * @param value
+     */
+    private void detectAndChangeTypeValue(Object value) {
+        if (_jsonType == DataType.JsonTypes.UNKNOWN) {
+            switch (DataType.getDataType(value)) {
+                case DOUBLE:
+                    _jsonType = DataType.JsonTypes.DOUBLE;
+                    break;
+                case INTEGER:
+                    _jsonType = DataType.JsonTypes.INTEGER;
+                    break;
+            }
+        } else if (_jsonType == DataType.JsonTypes.INTEGER &&
+                DataType.getDataType(value) == DataType.JsonTypes.DOUBLE) {
+            _doubleResult = _intResult + 0.0;
+            _intResult = 0;
+        }
     }
 
     public static Object isValidAggregation(Object agg) {
