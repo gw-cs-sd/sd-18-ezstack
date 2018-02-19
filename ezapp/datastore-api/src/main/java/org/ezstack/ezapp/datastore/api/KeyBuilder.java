a0372a42acdc9dc33b515bef4adc6c477d2026b4
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2018-02-19 13:10:53 -0800

    minor change in hash

--- ezapp/datastore-api/src/main/java/org/ezstack/ezapp/datastore/api/KeyBuilder.java
+++ ezapp/datastore-api/src/main/java/org/ezstack/ezapp/datastore/api/KeyBuilder.java
@@ -1,12 +1,11 @@
 package org.ezstack.ezapp.datastore.api;
 
+import com.google.common.base.Charsets;
 import com.google.common.hash.Hashing;
-
-import java.nio.charset.Charset;
 
 public class KeyBuilder {
     public static String hashKey(String table, String key) {
         StringBuilder sb = new StringBuilder(table).append("~").append(key);
-        return Hashing.murmur3_128().hashString(sb.toString(), Charset.defaultCharset()).toString();
+        return Hashing.murmur3_128().hashString(sb.toString(), Charsets.UTF_8).toString();
     }
 }
