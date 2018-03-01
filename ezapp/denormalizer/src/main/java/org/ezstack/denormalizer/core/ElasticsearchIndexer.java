b318adf8d43bb19a0bb11afba498ee419efd1071
Ben Devore <bdevore@gwu.edu>
2018-03-01 14:00:01 -0500

    make ES indexer system name configurable (#44)

--- ezapp/denormalizer/src/main/java/org/ezstack/denormalizer/core/ElasticsearchIndexer.java
+++ ezapp/denormalizer/src/main/java/org/ezstack/denormalizer/core/ElasticsearchIndexer.java
@@ -14,6 +14,12 @@ public class ElasticsearchIndexer implements SinkFunction<WritableResult> {
 
     private static final ObjectMapper _mapper = new ObjectMapper();
 
+    private final String _clusterName;
+
+    public ElasticsearchIndexer(String clusterName) {
+        _clusterName = clusterName;
+    }
+
     private String getESDeletePath(String index, String type, int version) {
         return String.format("%s/%s/delete/internal/%d", index, type, version);
     }
@@ -28,7 +34,7 @@ public class ElasticsearchIndexer implements SinkFunction<WritableResult> {
         switch (resultMsg.getOpCode()) {
             case INDEX:
                 messageCollector.send(new OutgoingMessageEnvelope(
-                        new SystemStream("elasticsearch",
+                        new SystemStream(_clusterName,
                                 getESUpdatePath(resultMsg.getTable(), resultMsg.getTable(), resultMsg.getDocument().getVersion())),
                         resultMsg.getDocument().getKey(), _mapper.convertValue(resultMsg.getDocument(), Map.class)));
                 break;
@@ -36,7 +42,7 @@ public class ElasticsearchIndexer implements SinkFunction<WritableResult> {
                 // Deletes must specify a version number to prevent the race condition where it accidentally deletes
                 // an already updated document from a different container
                 messageCollector.send(new OutgoingMessageEnvelope(
-                        new SystemStream("elasticsearch",
+                        new SystemStream(_clusterName,
                                 getESDeletePath(resultMsg.getTable(), resultMsg.getTable(), resultMsg.getDocument().getVersion())),
                         resultMsg.getDocument().getKey(), _mapper.convertValue(resultMsg.getDocument(), Map.class)));
                 break;
