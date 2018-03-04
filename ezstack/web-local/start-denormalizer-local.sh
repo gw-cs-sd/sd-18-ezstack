5c30d3bd3d3d05345bd6d59a888d3b0dd014e40f
Ben Devore <bdevore@gwu.edu>
2018-03-04 18:07:20 -0500

    reconfigure the denormalizer to work on yarn (#46)

--- ezstack/web-local/start-denormalizer-local.sh
+++ ezstack/web-local/start-denormalizer-local.sh
@@ -9,8 +9,8 @@ DENORMALIZER_DIR=${BASE_DIR}/denormalizer
 #java -Dlog4j.configuration=file:${DIR}/log4j-console.xml -Dsamza.log.dir=${DIR} -jar ${DENORMALIZER_DIR}/target/ezapp-denormalizer-${VERSION}-SNAPSHOT.jar -config-path ${DENORMALIZER_DIR}/src/main/config/fanout-app-local-runner.properties --config-factory org.apache.samza.config.factories.PropertiesConfigFactory
 
 mkdir -p ${DIR}/deploy/denormalizer
-pushd ${DIR}/deploy/denormalizer
+tar -xvf ${BASE_DIR}/denormalizer/target/ezstack-denormalizer-0.1-SNAPSHOT-dist.tar.gz -C deploy/denormalizer
 
-java -Dlog4j.configuration=file:${DIR}/log4j-console.xml -Dsamza.log.dir=${DIR} -jar ${DENORMALIZER_DIR}/target/ezapp-denormalizer-${VERSION}-SNAPSHOT.jar -config-path ${DENORMALIZER_DIR}/src/main/config/document-resolver-local-app.properties --config-factory org.apache.samza.config.factories.PropertiesConfigFactory
+${DIR}/deploy/denormalizer/bin/run-app.sh --config-factory=org.apache.samza.config.factories.PropertiesConfigFactory --config-path=file://${DIR}/deploy/denormalizer/config/denormalizer-yarn.properties
+#java -Dlog4j.configuration=file:${DIR}/log4j-console.xml -Dsamza.log.dir=${DIR} -jar ${DENORMALIZER_DIR}/target/ezapp-denormalizer-${VERSION}-SNAPSHOT.jar -config-path ${DENORMALIZER_DIR}/src/main/config/document-resolver-local-app.properties --config-factory org.apache.samza.config.factories.PropertiesConfigFactory
 
-popd
