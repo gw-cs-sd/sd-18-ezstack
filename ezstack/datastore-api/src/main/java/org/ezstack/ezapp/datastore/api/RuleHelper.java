d20b9fb24af8c82a3921dd27fa62241462a0e41f
Beshoi Genidy <bgenidy@gwmail.gwu.edu>
2018-03-19 21:13:55 -0700

    rule helper implementation

--- ezstack/datastore-api/src/main/java/org/ezstack/ezapp/datastore/api/RuleHelper.java
+++ ezstack/datastore-api/src/main/java/org/ezstack/ezapp/datastore/api/RuleHelper.java
@@ -1,29 +1,59 @@
 package org.ezstack.ezapp.datastore.api;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class RuleHelper {
-
     /**
-     * @param query
-     * @return a modified query that can be made into a rule or null if query doesn't qualify
+     * Query - will return the new query that can be safely passed as a rule for the denormalizer.
+     * null - will be returned if the original query is not possible to be converted into a rule.
+     * @param q
+     * @return
      */
-    public static Query generateRuleCompliantQuery(Query query) {
-        // Queries must be at least 2 levels long
-        if (query.getJoin() == null || query.getJoin().getJoin() != null) {
+    public static Query getRuleQuery(Query q) {
+        if (!isTwoLevelQuery(q)) {
             return null;
         }
 
-        Query innerQ = query.getJoin();
+        return new Query(
+                null,
+                q.getTable(),
+                q.getFilters(),
+                q.getJoin(),
+                q.getJoinAttributeName(),
+                q.getJoinAttributes(),
+                q.getExcludeAttributes(),
+                q.getIncludeAttributes()
+        );
+    }
 
-        List<SearchType> searchTypes = null;
-        if (QueryHelper.hasSearchRequest(query.getSearchTypes())) {
-            searchTypes = new LinkedList<>();
-            searchTypes.add(new SearchType(SearchType.Type.SEARCH.toString(), null));
+    /**
+     * Given the original user query and a query received from a rule it will return a boolean
+     * True - indicates they are a close enough match
+     * False - not a match, move on.
+     * @param original
+     * @param rule
+     * @return
+     */
+    public static boolean ruleQueryMatch(Query original, Query rule) {
+        if (!isTwoLevelQuery(original) || !isTwoLevelQuery(rule)) {
+            return false;
         }
 
-        // TODO
-        return null;
+        if (!original.getTable().equals(rule.getTable())) return false;
+        if (!original.getFilters().equals(rule.getFilters())) return false;
+        if (!original.getJoin().equals(rule.getJoin())) return false;
+        if (!original.getJoinAttributes().equals(rule.getJoinAttributes())) return false;
+        if (!original.getExcludeAttributes().equals(rule.getExcludeAttributes())) return false;
+        if (!original.getIncludeAttributes().equals(rule.getIncludeAttributes())) return false;
+        return true;
     }
+
+    /**
+     * True - if the query is a two level query
+     * False - if query is not a two level query
+     * @param q
+     * @return
+     */
+    public static boolean isTwoLevelQuery(Query q) {
+        return q.getJoin() != null && q.getJoin().getJoin() == null;
+    }
+
 }
