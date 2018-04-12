683904621139b28ab143f2f52387bb7c7e64b1f1
bdevore17 <bdevore17@gmail.com>
2018-04-12 00:29:49 -0400

    update denormalizer yarn configs

--- ezstack/web-local/start-denormalizer-local.sh
+++ ezstack/web-local/start-denormalizer-local.sh
@@ -11,6 +11,6 @@ DENORMALIZER_DIR=${BASE_DIR}/denormalizer
 mkdir -p ${DIR}/deploy/denormalizer
 pushd ${DIR}/deploy/denormalizer
 
-java -Dlog4j.configuration=file:${DIR}/log4j-console.xml -Dsamza.log.dir=${DIR} -jar ${DENORMALIZER_DIR}/target/ezstack-denormalizer-${VERSION}-SNAPSHOT.jar -config-path ${DENORMALIZER_DIR}/src/main/config/document-resolver-local-app.properties --config-factory org.apache.samza.config.factories.PropertiesConfigFactory
+java -Dlog4j.configuration=file:${DIR}/log4j-console.xml -Dsamza.log.dir=${DIR} -jar ${DENORMALIZER_DIR}/target/ezstack-denormalizer-${VERSION}-SNAPSHOT.jar -config-path ${DENORMALIZER_DIR}/src/main/config/denormalizer-local.properties --config-factory org.apache.samza.config.factories.PropertiesConfigFactory
 
 popd
